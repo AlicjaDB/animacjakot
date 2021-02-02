09aee13065d699d25a904f25663b8d8563f06cbb
AlicjaDB <fiboza2@gmail.com>
2021-02-02 18:03:17 +0100

    https://github.com/AlicjaDB/animacjakot.git

--- js/cat.js
+++ js/cat.js
@@ -1,16 +1,19 @@
 let btns = document.querySelectorAll(".buttons button");
 let cat = document.getElementsByClassName("cat")[0];
-click1 =btns[0];
-click2=btns[1];
 
-click1.onclick = kitty;
-click2.onclick= tom;
+
 function kitty(){
 cat.classList.add("blue-eyes");
 }
 console.log(cat.classList.contains("blue-eyes"));
 
 function tom(){
-  cat.classList.toggle('blue-eyes');
+cat.classList.toggle('blue-eyes');
 }
 console.log(cat.classList.contains("blue-eyes"));
+
+click1= btns[0];
+click2= btns[1];
+
+click1.addEventListener("click", kitty);
+click2.addEventListener("click", tom);
